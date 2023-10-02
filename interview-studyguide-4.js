ffc227460c612f948fb419374090b8cf4b767cc4
stevegebhardt <gebhardt.steven3@gmail.com>
2023-10-02 11:22:16 -0400

    committing setup files

---
+++ interview-studyguide-4.js
@@ -0,0 +1,23 @@
+/* 
+Using an api endpoint like this: https://pokeapi.co/api/v2/pokemon/ditto
+
+Write a function that takes a pokemon name and gives back an object similar to this:
+{
+  name: "ditto",
+  abilities: [{},{}],
+  height: 3,
+  base_experience: 48
+}
+
+If you don't know any pokemon names to test this with, here are some to get you started:
+pikachu
+charmander
+bulbasaur
+*/
+
+
+function catchEmAll(pokemonName) {
+  
+}
+
+catchEmAll("pikachu")
